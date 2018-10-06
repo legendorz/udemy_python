163b641ff88dbe77e7f6c7d41c33ca23f7c6c05b
legendorz <legend.orz@gmail.com>
2018-10-06 21:10:11 +0800

    Update udemy_hw.py
    挑戰二

--- udemy_hw.py
+++ udemy_hw.py
@@ -65,5 +65,74 @@ def three_days(data):
     return info
 
 
+#挑戰二：實際運用策略計算報酬總金額計每次成交狀況繪製圖
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_csv(file):
+    # converters 那邊是告訴它Adj Close那欄 我要讀取成float，否則他預設會是字串。
+    df = pd.read_csv(file) 
+    df = df['Adj Close'].astype('float') # 我只要Adj Close那欄的資料
+    return df.tolist() # 轉換回python內建的list物件
+
+
+def calc_profit(data, signal):
+    pos = 0 # 持有方向
+    entry = 0 # 進場價
+    trades = [] # 裝著每筆交易的損益
+    for i in range(len(data)):
+        if signal[i] == 1:
+            if pos == 0: # 表示目前沒有持股
+                entry = data[i] # 那就只是單純進場，紀錄成本價就好
+            elif pos == -1: # 原本持有空單，現在遇到買進訊號
+                # 因為要把空單出場，換成多單
+                # 要先計算 此筆單出場的獲利
+                profit = entry - data[i] # 空單的獲利是 成本價 - 現在價格
+                trades.append(profit)
+                entry = data[i]
+            pos = 1 # 把持有方向 設為 1
+        elif signal[i] == -1:
+            if pos == 0: # 表示目前沒有持股
+                entry = data[i] # 那就只是單純進場，紀錄成本價就好
+            elif pos == 1: # 原本持有多單，現在遇到賣出訊號
+                # 因為要把多單出場，換成空單
+                # 要先計算 此筆單出場的獲利
+                profit = data[i] - entry  # 空單的獲利是 成本價 - 現在價格
+                trades.append(profit)
+                entry = data[i]
+            pos = -1 # 把持有方向 設為 1
+
+    return trades
+
+
+def calc_equity(trades):
+    equities = []
+    equity = 0
+    for trade in trades:
+        equity += trade
+        equities.append(equity)
+    return equities
+
+
+def graph_equity(equity):
+    plt.plot(equity)
+    plt.ylabel('equity')
+    plt.xlabel('trades')
+    plt.show()
+
+
+def main():
+    data = read_csv('2330.csv')
+    signal = three_days(data)
+    trades = calc_profit(data, signal)
+    total = sum(trades) * 1000 # 放大一千倍因為每次交易都是一張(1000股)
+    print('總共損益為', total)
+    print('一共有', len(trades), '筆交易')
+    equity = calc_equity(trades)
+    graph_equity(equity)
+
+
+main()
 
 
